cf2f26a9cafff781c3178cbbc5879abb6e33319d
bram <bramas16@gmail.com>
2018-05-19 00:17:08 +0700

    modif dikit

--- resources/views/webpage/sertifikat.blade.php
+++ resources/views/webpage/sertifikat.blade.php
@@ -14,7 +14,9 @@
 			<div class="col-sm-2"></div><br><br><br>
 			<div class="col-sm-2"></div>
 			<div class="col-sm-8 w3-center">
-				<img style="width: 95%" id="sertif" src="">
+				<a href="/" download>
+					<img style="width: 95%" id="sertif" src="">
+				</a>
 			</div>
 			<div class="col-sm-2"></div>
 		</div>
@@ -25,6 +27,7 @@
 			var temp = "{{$user->name}}";
 			var temp2 = temp.replace(/ /g, "%20");
 			$("#sertif").attr('src','/image/'+temp2+'.jpg');
+			$("a").attr('href','/image/'+temp2+'.jpg');
 		});
 	</script>
 @endsection()
